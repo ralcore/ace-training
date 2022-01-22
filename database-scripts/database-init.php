7e8ab87b1d29e6c96583ba24e39b7cfc74c4b818
Ralcore <mlipytino@hotmail.co.uk>
2022-01-22 20:46:10 +0000

    added script database initialisation script.

---
+++ database-scripts/database-init.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$conn = mysqli_connect("localhost", "root", "root");
+$sql = "CREATE DATABASE aceTraining";
+mysqli_query ($conn,$sql) or die(mysqli_error($conn));
+?>
