0b03be9463a4182c0dca186b7190b6d600c06f92
ralcore <mlipytino@hotmail.co.uk>
2022-02-02 14:40:33 +0000

    created assignment view page + added progress bar

---
+++ assignmentview.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tutor Assignment View</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link href="css/assignmentview.css" rel="stylesheet">
+        <script src="js/assignmentview.js"></script>
+    </head>
+
+    <body>
+        <div class="container-fluid" style="max-width: 640px;">
+            <div class="row rounded">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <!-- completion dial -->
+                            <div class="progress mx-auto" data-value='53'>
+                                <span class="progress-left">
+                                    <span class="progress-bar border-danger"></span>
+                                </span>
+                                <span class="progress-right">
+                                    <span class="progress-bar border-danger"></span>
+                                </span>
+                                <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                                    <div class="h2 font-weight-bold">53%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <!-- completion dial -->
+                            <div class="progress mx-auto" data-value='53'>
+                                <span class="progress-left">
+                                    <span class="progress-bar border-danger"></span>
+                                </span>
+                                <span class="progress-right">
+                                    <span class="progress-bar border-danger"></span>
+                                </span>
+                                <div class="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+                                    <div class="h2 font-weight-bold">53%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6"></div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
